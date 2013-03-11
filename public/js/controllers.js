2acfcfacb8efc0c84b9bca13d6768650fbed61c4
Robert DeSimone <icenine457@gmail.com>
2013-03-10 20:16:48 -0400

    Add pagination, but need to fix copypasta

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -56,20 +56,102 @@ function ImportMemberCtrl($scope, $http, $location) {
 
 function MembersCtrl($scope, $http, $location) {
   $scope.$emit('changeTab');
-  $scope.form = {};
-  $http.get('/api/members', $scope.form).
-    success(function(data) {
-      $scope.members = data;
-      $scope.memberHeader = "There " + ( data.length == 1 ? "is " : "are ") + (data.length > 0 ? data.length : "no") + " member" + (data.length != 1 ? "s." : ".");
-    });
+  $scope.page = 0;
+  $scope.perPage = 10;
+  $scope.list = function() {
+    $http.get('/api/members/' + $scope.page + '/' + $scope.perPage).
+      success(function(data) {
+        $scope.members = data.members;
+        $scope.membersTotal = data.membersTotal;
+        $scope.memberHeader = "There " + ( $scope.membersTotal == 1 ? "is " : "are ") + ($scope.membersTotal > 0 ? $scope.membersTotal : "no") + " member" + ($scope.membersTotal != 1 ? "s." : ".");
+        $scope.visiblePages = function() {
+          var allPages = Math.ceil($scope.membersTotal / $scope.perPage);
+          var visible = [];
+
+          // I weep for not having a .. method
+          if ($scope.page < 4) { return [0,1,2,3,4,5,6,7,8,9] };
+          for (var pppp = $scope.page - 4; pppp < $scope.page + 6; pppp++) {
+            if (pppp < allPages) visible.push(pppp);
+          }
+          return visible;
+        }
+        $scope.isPageSelected = function(page) {
+          if (page == $scope.page) {
+            return "active"
+          }
+        };
+      });
+    }
+  $scope.list();
+
+  $scope.navPage = function(page) {
+    if ($scope.page == page) return;
+    $scope.skip = page * $scope.perPage;
+    $scope.page = page;
+    $scope.list();
+  };
+  $scope.nextPage = function() {
+    if ($scope.page == Math.ceil($scope.membersTotal / $scope.perPage)) return;
+    ++$scope.page
+    $scope.skip = $scope.page * $scope.perPage;
+    $scope.list();
+  };
+  $scope.prevPage = function() {
+    if ($scope.page == 0) return;
+    --$scope.page
+    $scope.skip = $scope.page * $scope.perPage;
+    $scope.list();
+  };
 };
 
 function SearchCtrl($scope, $http, $location, $routeParams) {
   $scope.$emit('changeTab');
-  $http.get('/api/members', $scope.form).
-    success(function(data) {
-      $scope.members = data;
-    });
+  $scope.page = 0;
+  $scope.perPage = 10;
+  $scope.list = function() {
+    $http.get('/api/members/' + $scope.page + '/' + $scope.perPage).
+      success(function(data) {
+        $scope.members = data.members;
+        $scope.membersTotal = data.membersTotal;
+        $scope.memberHeader = "There " + ( $scope.membersTotal == 1 ? "is " : "are ") + ($scope.membersTotal > 0 ? $scope.membersTotal : "no") + " member" + ($scope.membersTotal != 1 ? "s." : ".");
+        $scope.visiblePages = function() {
+          var allPages = Math.ceil($scope.membersTotal / $scope.perPage);
+          var visible = [];
+
+          // I weep for not having a .. method
+          if ($scope.page < 4) { return [0,1,2,3,4,5,6,7,8,9] };
+          for (var pppp = $scope.page - 4; pppp < $scope.page + 6; pppp++) {
+            if (pppp < allPages) visible.push(pppp);
+          }
+          return visible;
+        }
+        $scope.isPageSelected = function(page) {
+          if (page == $scope.page) {
+            return "active"
+          }
+        };
+      });
+    }
+  $scope.list();
+
+  $scope.navPage = function(page) {
+    if ($scope.page == page) return;
+    $scope.skip = page * $scope.perPage;
+    $scope.page = page;
+    $scope.list();
+  };
+  $scope.nextPage = function() {
+    if ($scope.page == Math.ceil($scope.membersTotal / $scope.perPage)) return;
+    ++$scope.page
+    $scope.skip = $scope.page * $scope.perPage;
+    $scope.list();
+  };
+  $scope.prevPage = function() {
+    if ($scope.page == 0) return;
+    --$scope.page
+    $scope.skip = $scope.page * $scope.perPage;
+    $scope.list();
+  };
 
   $scope.getDate = function(dateStr) {
     return new Date(dateStr);
--- public/js/filters.js
+++ public/js/filters.js
@@ -2,11 +2,18 @@
 
 // Filters {{{
 
-angular.module('alsbooks.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
+var alsbooks = angular.module('alsbooks.filters', [])
+alsbooks.filter('interpolate', function(version) {
+  return function(text) {
+    return String(text).replace(/\%VERSION\%/mg, version);
+  }
+});
+alsbooks.filter('range', function() {
+  return function(input, total) {
+    total = parseInt(total);
+    for (var i=0; i<total; i++) input.push(i);
+    return input;
+  };
+});
 
 // }}}
