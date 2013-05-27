a46a537181b29ef4a0ed1b3b4db74e2b728d2436
Robert DeSimone <icenine457@gmail.com>
2013-05-27 12:51:53 +0000

    Fix login failures

--- public/js/controllers/LoginCtrl.js
+++ public/js/controllers/LoginCtrl.js
@@ -6,8 +6,12 @@ function LoginCtrl($scope, $http, $location, $cookies, $rootScope) {
   };
   $scope.login = function() {
     $http.post('/api/users/login', $scope.user).
-      success(function(loggedIn) {
-        if (loggedIn) {
+      success(function(res) {
+        if (res.errors) {
+          $scope.errors = res.errors;
+          return;
+        }
+        if (res.success) {
           $rootScope.$broadcast('login');
         }
       })
