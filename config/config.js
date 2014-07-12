c1667b2c6115c87d34cab127966a57c217fe02a5
Robert DeSimone <icenine457@gmail.com>
2014-07-12 17:15:58 +0000

    Ignore file

--- config/config.js
+++ config/config.js
@@ -17,6 +17,17 @@ module.exports = {
 
   }
   , production: {
-
+    root: require('path').normalize(__dirname + '/..'),
+    app: {
+      name: 'ALS Books'
+    },
+    db: 'mongodb://localhost/alsBooks',
+    googleApiKey: 'AIzaSyDrgLtAS6CBk9nw_6fnOlog_z2wfjLEHzU',
+    contactDetails: {
+      user:     "alspubtracker@gmail.com"
+     , password: "DaKyba4!"
+     , host:     "smtp.gmail.com"
+     , ssl:      true
+    }
   }
 }
